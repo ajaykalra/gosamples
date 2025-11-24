f6dbf34cdf82ba4c5f757ff00cbbabacc2d32ec0
Ajay Kalra <ajkalra@gmail.com>
2025-11-24 00:39:02 -0500

    more

--- 05_db_access/main.go
+++ 05_db_access/main.go
@@ -11,7 +11,7 @@ import (
 const (
 	host     = "localhost"
 	port     = 5432
-	user     = "admin"
+	user     = "postgres"
 	password = "admin"
 	dbname   = "products"
 )
@@ -24,6 +24,57 @@ type Product struct {
 }
 
 func main() {
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	// 	host, port, user, password, dbname)
+
+	// // Use the connection string to open a DB handle.
+	// db, err := sql.Open("postgres", psqlInfo)
+	// if err != nil {
+	// 	log.Fatalf("sql.Open: %v", err)
+	// }
+	// defer db.Close()
+
+	// // Verify connection with Ping.
+	// if err := db.Ping(); err != nil {
+	// 	log.Fatalf("db.Ping: %v", err)
+	// }
+
+	// fmt.Println("Connected to Postgres")
+
+	// // Simple query to demonstrate use of the DB handle.
+	// var now string
+	// if err := db.QueryRow("SELECT NOW()::text").Scan(&now); err != nil {
+	// 	log.Fatalf("query NOW(): %v", err)
+	// }
+	// fmt.Printf("Postgres time: %s\n", now)
+
+	// rows, err := db.Query("SELECT product_id , product_name, price, stock_quantity FROM products")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
+
+	// var products []Product
+	// for rows.Next() {
+	// 	var product Product
+	// 	if err := rows.Scan(&product.ID, &product.ProductName, &product.Price, &product.Quantity); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(product.ID, product.ProductName, product.Price, product.Quantity)
+	// 	products = append(products, product)
+	// }
+	// if err := rows.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Products:", products)
+
+	var products []Product
+	var err error
+	products, err = getProducts()
+	fmt.Println("Products:", products, "error:", err)
+}
+
+func getProducts() ([]Product, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
@@ -31,12 +82,14 @@ func main() {
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatalf("sql.Open: %v", err)
+		return nil, err
 	}
 	defer db.Close()
 
 	// Verify connection with Ping.
 	if err := db.Ping(); err != nil {
 		log.Fatalf("db.Ping: %v", err)
+		return nil, err
 	}
 
 	fmt.Println("Connected to Postgres")
@@ -45,12 +98,14 @@ func main() {
 	var now string
 	if err := db.QueryRow("SELECT NOW()::text").Scan(&now); err != nil {
 		log.Fatalf("query NOW(): %v", err)
+		return nil, err
 	}
 	fmt.Printf("Postgres time: %s\n", now)
 
 	rows, err := db.Query("SELECT product_id , product_name, price, stock_quantity FROM products")
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 	defer rows.Close()
 
@@ -65,6 +120,9 @@ func main() {
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 	fmt.Println("Products:", products)
+
+	return products, nil
 }
